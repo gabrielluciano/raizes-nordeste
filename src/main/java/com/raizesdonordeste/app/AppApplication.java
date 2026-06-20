0a86742129d9fbe15f33f105416362101d61cee5
Gabriel Luciano <gabriel-luciano@outlook.com>
2026-06-20 15:54:19 -0300

    feat: criação do projeto Spring Boot

---
+++ src/main/java/com/raizesdonordeste/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.raizesdonordeste.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
